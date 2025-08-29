b8c1edffad11a679133829dff273e42652452834
Bora Caglayan 00555942 <bora.caglayan@huawei.com>
2025-08-29 17:08:34 +0100

    Updated model

--- simple_example.py
+++ simple_example.py
@@ -13,7 +13,7 @@ variable_names = list(df.columns)
 
 # Model loading and prediction
 models_path = 'res'
-model_name = 'LCM_CI_9.6M_joint_220k_permuted_3' 
+model_name = 'lcm_CI_RH_12_3_merged_290k' 
 
 model = CausalModel(model_name = model_name, model_path = Path(models_path) / f"{model_name}.ckpt") #Architecture_PL.load_from_checkpoint(Path(models_path) / f"{model_name}.ckpt")
 pred = model.predict(df, max_lag_to_predict = 1)
